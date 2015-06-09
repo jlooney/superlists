bf47614d12761afe7d00713afda57a09f3c1d5f0
unknown <julia.looney@austin.utexas.edu>
2015-06-09 11:05:35 -0500

    Add a URL for creating a new list via POST

--- superlists/urls.py
+++ superlists/urls.py
@@ -3,8 +3,9 @@ from lists import views
 
 urlpatterns = [
     # Examples:
-    url(r'^$', views.home_page, name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'lists.views.home_page', name='home'),
+	url(r'^lists/new$', views.new_list, name='new_list'),
+	url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
     #url(r'^admin/', include(admin.site.urls)),
 	
 ]
