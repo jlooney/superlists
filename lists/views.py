08c32195f0425da77b13385a841061715abe9383
unknown <julia.looney@austin.utexas.edu>
2015-06-23 10:18:04 -0500

    enforce model validation in list view

--- lists/views.py
+++ lists/views.py
@@ -2,27 +2,38 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
+from django.utils.html import escape
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def new_list(request):
-	list_ = List.objects.create()
-	item = Item.objects.create(text=request.POST['item_text'],list=list_)
-	try:
-		item.full_clean()
-		item.save()
-	except ValidationError:
-		list_.delete()
-		error = "You can't have an empty list item"
-		return render(request, 'home.html', {"error": error})
-	return redirect('/lists/%d/' % (list_.id,))
-	
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = escape("You can't have an empty list item")
+        return render(request, 'home.html', {"error": error})
+    return redirect('/lists/%d/' % (list_.id,))
+
+
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'], list=list_)
-		return redirect('/lists/%d/' % (list_.id,))
-	return render(request, 'list.html', {'list': list_})
-	
+    list_ = List.objects.get(id=list_id)
+    error = None
+
+    if request.method == 'POST':
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = escape("You can't have an empty list item")
+
+    return render(request, 'list.html', {'list': list_, 'error' : error})
+
